70bd9ba7e3c3d1b507a68b57450fce54dee4b352
Marta <mpopielska1@st.swp0s.edu.pl>
2024-12-08 13:14:05 +0100

    lab_03_zadania

--- folder_projektu/folder_aplikacji/views.py
+++ folder_projektu/folder_aplikacji/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def welcome_view(request):
+    now = datetime.datetime.now()
+    html = f"""
+        <html><body>
+        Witaj użytkowniku! </br>
+        Aktualna data i czas na serwerze: {now}.
+        </body></html>"""
+    return HttpResponse(html)
